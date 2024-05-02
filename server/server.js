00f0926dc53eef0f776f9dd532fbbb25b6814d87
Tristan Jin <tjin368@gmail.com>
2024-05-01 23:25:43 -0400

    Update CORS to allow deployed url

--- server/server.js
+++ server/server.js
@@ -16,8 +16,19 @@ const PORT = process.env.PORT || 3003;
 // NODE JS SERVER SETUP ##################################################
 const app = express();
 app.use(express.json());
+
+const allowedOrigins = ['http://localhost:3000', 'https://nick-and-tash-wedding.web.app'];
 app.use(cors({
-  origin: 'http://localhost:3000'
+  origin: function(origin, callback) {
+    // allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
 }));
 
 // Connect to MongoDB
